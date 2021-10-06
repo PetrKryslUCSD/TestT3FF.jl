476978ddff5bc64d5f065c052b260a352ac25cfa
Petr Krysl <pkrysl@ucsd.edu>
2021-10-05 20:58:23 -0700

    additional examples

--- test/runtests.jl
+++ test/runtests.jl
@@ -4,6 +4,9 @@
 for s in [
     "barrel_vault_examples",
     "clamped_hypar_examples", 
+    "pinched_cylinder_examples",
+    "ss_circular_plate_udl_examples",
+    "hemisphere_open_examples", 
     "raasch_examples"
     ]
     include(s * ".jl")
