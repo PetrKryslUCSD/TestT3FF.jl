14bf5910c117a3a51f944aa23cad8eca62b429d8
Petr Krysl <pkrysl@ucsd.edu>
2022-03-01 09:40:30 -0800

    a few more data points

--- test/scordelis_lo_examples.jl
+++ test/scordelis_lo_examples.jl
@@ -30,40 +30,43 @@ using FinEtools.MeshExportModule.VTKWrite: vtkwrite
 
 # analytical solution for the vertical deflection and the midpoint of the
 # free edge 
-analyt_sol=-0.3024;
+analyt_sol = -0.3024
 # Parameters:
-E=4.32e8;
-nu=0.0;
-thickness = 0.25; # geometrical dimensions are in feet
-R = 25.0;
-L = 50.0;
+E = 4.32e8
+nu = 0.0
+thickness = 0.25 # geometrical dimensions are in feet
+R = 25.0
+L = 50.0
 
 cylindrical!(csmatout::FFltMat, XYZ::FFltMat, tangents::FFltMat, fe_label::FInt) = begin
-    r = vec(XYZ); r[2] = 0.0; r[3] += R
-    csmatout[:, 3] .= vec(r)/norm(vec(r))
+    r = vec(XYZ)
+    r[2] = 0.0
+    r[3] += R
+    csmatout[:, 3] .= vec(r) / norm(vec(r))
     csmatout[:, 2] .= (0.0, 1.0, 0.0) #  this is along the axis
     cross3!(view(csmatout, :, 1), view(csmatout, :, 2), view(csmatout, :, 3))
     return csmatout
 end
 
 function _execute_dsg_model(formul, n = 8, visualize = true)
-    
-    
-    tolerance = R/n/10
+
+
+    tolerance = R / n / 10
     # fens, fes = T3blockrand(40/360*2*pi,L/2,n,n);
-    fens, fes = T3block(40/360*2*pi,L/2,n,n,:b);
+    fens, fes = T3block(40 / 360 * 2 * pi, L / 2, n, n, :b)
     fens.xyz = xyz3(fens)
     for i in 1:count(fens)
-        a=fens.xyz[i, 1]; y=fens.xyz[i, 2];
-        fens.xyz[i, :] .= (R*sin(a), y, R*(cos(a)-1))
+        a = fens.xyz[i, 1]
+        y = fens.xyz[i, 2]
+        fens.xyz[i, :] .= (R * sin(a), y, R * (cos(a) - 1))
     end
 
     mater = MatDeforElastIso(DeforModelRed3D, E, nu)
     ocsys = CSys(3, 3, cylindrical!)
-    
+
     sfes = FESetShellT3()
     accepttodelegate(fes, sfes)
-    femm = formul.make(IntegDomain(fes, TriRule(1), thickness),  mater)
+    femm = formul.make(IntegDomain(fes, TriRule(1), thickness), mater)
     femm.drilling_stiffness_scale = 1.0e-4
     femm.mult_el_size = 0.2
     stiffness = formul.stiffness
@@ -72,43 +75,43 @@ function _execute_dsg_model(formul, n = 8, visualize = true)
     # Construct the requisite fields, geometry and displacement
     # Initialize configuration variables
     geom0 = NodalField(fens.xyz)
-    u0 = NodalField(zeros(size(fens.xyz,1), 3))
+    u0 = NodalField(zeros(size(fens.xyz, 1), 3))
     Rfield0 = initial_Rfield(fens)
-    dchi = NodalField(zeros(size(fens.xyz,1), 6))
+    dchi = NodalField(zeros(size(fens.xyz, 1), 6))
 
     # Apply EBC's
     # rigid diaphragm
     l1 = selectnode(fens; box = Float64[-Inf Inf 0 0 -Inf Inf], inflate = tolerance)
-    for i in [1,3,5]
+    for i in [1, 3, 5]
         setebc!(dchi, l1, true, i)
     end
     # plane of symmetry perpendicular to Y
-    l1 = selectnode(fens; box = Float64[-Inf Inf L/2 L/2 -Inf Inf], inflate = tolerance)
-    for i in [2,4,6]
+    l1 = selectnode(fens; box = Float64[-Inf Inf L / 2 L / 2 -Inf Inf], inflate = tolerance)
+    for i in [2, 4, 6]
         setebc!(dchi, l1, true, i)
     end
     # plane of symmetry perpendicular to X
     l1 = selectnode(fens; box = Float64[0 0 -Inf Inf -Inf Inf], inflate = tolerance)
-    for i in [1,5,6]
+    for i in [1, 5, 6]
         setebc!(dchi, l1, true, i)
     end
     applyebc!(dchi)
-    numberdofs!(dchi);
+    numberdofs!(dchi)
 
     # Assemble the system matrix
     associategeometry!(femm, geom0)
-    K = stiffness(femm, geom0, u0, Rfield0, dchi);
+    K = stiffness(femm, geom0, u0, Rfield0, dchi)
 
     # Midpoint of the free edge
-    nl = selectnode(fens; box = Float64[sin(40/360*2*pi)*25 sin(40/360*2*pi)*25 L/2 L/2 -Inf Inf], inflate = tolerance)
+    nl = selectnode(fens; box = Float64[sin(40 / 360 * 2 * pi) * 25 sin(40 / 360 * 2 * pi) * 25 L / 2 L / 2 -Inf Inf], inflate = tolerance)
     lfemm = FEMMBase(IntegDomain(fes, TriRule(3)))
-    fi = ForceIntensity(FFlt[0, 0, -90, 0, 0, 0]);
-    F = distribloads(lfemm, geom0, dchi, fi, 3);
-    
+    fi = ForceIntensity(FFlt[0, 0, -90, 0, 0, 0])
+    F = distribloads(lfemm, geom0, dchi, fi, 3)
+
     # Solve
-    U = K\F
+    U = K \ F
     scattersysvec!(dchi, U[:])
-    result =   dchi.values[nl, 3][1]
+    result = dchi.values[nl, 3][1]
     @info "Solution: $(result), $(round(result/analyt_sol*100, digits = 4))%"
 
     # Visualization
@@ -142,9 +145,9 @@ function _execute_dsg_model(formul, n = 8, visualize = true)
 
         vtkwrite("scordelis_lo_examples-$(n)-uur.vtu", fens, fes; scalars = scalars, vectors = [("u", dchi.values[:, 1:3]), ("ur", dchi.values[:, 4:6])])
 
-        scattersysvec!(dchi, (L/8)/maximum(abs.(U)).*U)
+        scattersysvec!(dchi, (L / 8) / maximum(abs.(U)) .* U)
         update_rotation_field!(Rfield0, dchi)
-        plots = cat(plot_space_box([[0 0 -L/2]; [L/2 L/2 L/2]]),
+        plots = cat(plot_space_box([[0 0 -L / 2]; [L / 2 L / 2 L / 2]]),
             #plot_nodes(fens),
             plot_midsurface(fens, fes; x = geom0.values, facecolor = "rgb(12, 12, 123)"),
             plot_midsurface(fens, fes; x = geom0.values, u = dchi.values[:, 1:3], R = Rfield0.values);
@@ -161,7 +164,7 @@ function test_convergence(ns = [4, 6, 8, 16, 32], visualize = false)
     results = []
     for n in ns
         v = _execute_dsg_model(formul, n, visualize)
-        push!(results, v/(-0.3024)*100)
+        push!(results, v / (-0.3024) * 100)
     end
     return ns, results
 end
@@ -171,7 +174,7 @@ end # module
 using .scordelis_lo_examples
 
 # Visualized internal resultants
-ns, results = scordelis_lo_examples.test_convergence([2*16,], true)
+ns, results = scordelis_lo_examples.test_convergence([2 * 16,], true)
 
 
 # These results come from Table 9 of An efficient three‑node triangular
@@ -182,35 +185,35 @@ ns, results = scordelis_lo_examples.test_convergence([2*16,], true)
 # 4, 8, 16, 32
 
 Allman = [
-1.004
-0.987
-0.987
-0.988] .* 100
+    1.004
+    0.987
+    0.987
+    0.988] .* 100
 Cook = [
-0.907
-0.929
-0.950
-0.981] .* 100
+    0.907
+    0.929
+    0.950
+    0.981] .* 100
 Providas_Kattis = [
-0.734
-0.815
-0.873
-0.967] .* 100
+    0.734
+    0.815
+    0.873
+    0.967] .* 100
 Shin_Lee = [
-1.379
-1.023
-1.004
-missing] .* 100
+    1.379
+    1.023
+    1.004
+    missing] .* 100
 MITC3plus = [
-0.669
-missing
-0.857
-0.955] .* 100
+    0.669
+    missing
+    0.857
+    0.955] .* 100
 TMRFS = [
-0.924
-0.963
-0.974
-0.998
+    0.924
+    0.963
+    0.974
+    0.998
 ] .* 100
 
 using PGFPlotsX
@@ -219,18 +222,18 @@ objects = []
 
 ns, results = scordelis_lo_examples.test_convergence()
 
-compensate(r) = (0.3024/0.3006) .* r
+compensate(r) = (0.3024 / 0.3006) .* r
 all_results = [("Present", compensate(results), "*"), ("Allman", compensate(Allman), "x"), ("ProvKat", compensate(Providas_Kattis), "triangle"), ("Cook", compensate(Cook), "square"), ("SL", compensate(Shin_Lee), "o"), ("MITC3+", compensate(MITC3plus), "diamond")]
 
-for r in  all_results
+for r in all_results
     @pgf p = PGFPlotsX.Plot(
-    {
-    color = "black",
-    line_width  = 0.7, 
-    style = "solid",
-    mark = "$(r[3])"
-    },
-    Coordinates([v for v in  zip(ns, r[2]) if v[2] !== missing])
+        {
+            color = "black",
+            line_width = 0.7,
+            style = "solid",
+            mark = "$(r[3])"
+        },
+        Coordinates([v for v in zip(ns, r[2]) if v[2] !== missing])
     )
     push!(objects, p)
     push!(objects, LegendEntry("$(r[1])"))
@@ -243,7 +246,7 @@ end
         ylabel = "Normalized Displacement [ND]",
         # xmin = range[1],
         # xmax = range[2],
-        xmode = "linear", 
+        xmode = "linear",
         ymode = "linear",
         yminorgrids = "true",
         grid = "both",
@@ -265,7 +268,7 @@ pgfsave("scordelis_lo_examples-convergence.pdf", ax)
 
 ns, results = scordelis_lo_examples.test_convergence([16, 32, 64])
 q1, q2, q3 = results
-@show qtrue = (q2^2 - q1 * q3) / (2*q2 - q1 - q3)  *  0.3024/100
+@show qtrue = (q2^2 - q1 * q3) / (2 * q2 - q1 - q3) * 0.3024 / 100
 
 # ns, results = scordelis_lo_examples.test_convergence(8 .* [4, 8, 16])
 # q1, q2, q3 = results
@@ -289,11 +292,11 @@ q1, q2, q3 = results
 #  99.6820694134264                                                                       
 #  99.68801945695984      
 
-q1 =  -0.2885517078616122
-q2 =  -0.2978284489578761
-q3 =  -0.3003728234489632
-@show qtrue = (q2^2 - q1 * q3) / (2*q2 - q1 - q3)                                                                   
-                                                                                        
+q1 = -0.2885517078616122
+q2 = -0.2978284489578761
+q3 = -0.3003728234489632
+@show qtrue = (q2^2 - q1 * q3) / (2 * q2 - q1 - q3)
+
 # julia> @show qtrue = (q2^2 - q1 * q3) / (2*q2 - q1 - q3)                                
 # qtrue = (q2 ^ 2 - q1 * q3) / ((2q2 - q1) - q3) = 99.69011916380857                      
 # 99.69011916380857    
@@ -317,3 +320,25 @@ q3 =  -0.3003728234489632
 # # Not suitable for extrapolation: p=5, last three results
 # q1, q2, q3 = 0.300592456543583,   0.300592456635101,   0.300592456617171
 # @show qtrue = (q2^2 - q1 * q3) / (2*q2 - q1 - q3)
+
+# Abaqus, STRI3 elements, at regular mesh 64, 128, 256
+q1 = 0.2997
+q2 = 0.3003
+q3 = 0.3005
+@show qtrue = (q2^2 - q1 * q3) / (2 * q2 - q1 - q3) # 0.3006
+
+# Abaqus, S4 elements, regular mesh 64, 128, 256
+q1 = 0.3018
+q2 = 0.301976
+q3 = 0.302021
+@show qtrue = (q2^2 - q1 * q3) / (2 * q2 - q1 - q3) # 0.02036
+
+# T3FF, mesh with 128, 256, 512 elements per side
+# julia> ns, results = scordelis_lo_examples.test_convergence([16, 32, 64].*8)
+# [ Info: Scordelis-Lo shell
+# [ Info: Solution: -3.01162e-01, 9.95907e+01%
+# [ Info: Solution: -3.01378e-01, 9.96622e+01%
+# [ Info: Solution: -3.01441e-01, 9.96828e+01%
+q1, q2, q3 = [9.95907e+01, 9.96622e+01, 9.96828e+01]
+
+qtrue = (((q2 ^ 2 - q1 * q3) / ((2q2 - q1) - q3)) * 3.02400e-01) / 100 # = 3.01466e-01
